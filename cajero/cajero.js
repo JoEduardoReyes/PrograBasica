d3e23e5e866721e54dda872aa603a8174314c440
Eduardo Reyes <eduarno.1@hotmail.com>
2020-02-23 21:09:58 -0600

    Reparando los daños de cajero.html

--- cajero/cajero.js
+++ cajero/cajero.js
@@ -1,52 +1,98 @@
 class Billete {
   constructor(v, c) {
-    this.valor = v;
-    this.cantidad = c;
+    this.valor=v;
+    this.cantidad=c;
+    this.imagen = new Image;
+    this.imagen.src = this.valor + ".jpg"
+    this.cargaImagen=false;
   }
 }
 
-function entregarDinero() {
-  var t = document.getElementById("dinero");
-  dinero = parseInt(t.value);
-
-  for (var bi of cajas) {
-    if(dinero > 0){
-      div = Math.floor(dinero / bi.valor);
-      if (div > bi.cantidad) {
-        papeles = bi.cantidad;
-      }
-      else {
-        papeles = div;
-      }
-      entregado.push( new Billete(bi.valor, papeles));
-      dinero = dinero - (bi.valor * papeles);
-    }
-
-  }
-  if (dinero > 0) {
-    resultado.innerHTML = "No puedo darte esa cantidad";
-  }
-  else {
-    for(var e of entregado){
-      if (e.cantidad > 0) {
-      resultado.innerHTML += e.cantidad + " billetes de $" + e.valor + "<br />";
-      }
-    }
-  }
-}
-
-var cajas = [];
 var entregado = [];
-cajas.push( new Billete(100, 5));
-cajas.push( new Billete(50, 10));
-cajas.push( new Billete(20, 15));
-cajas.push( new Billete(10, 20));
-cajas.push( new Billete(5, 5));
+entregado.push(new Billete(200, 0));
+entregado.push(new Billete(100, 0));
+entregado.push(new Billete(50, 0));
+entregado.push(new Billete(20, 0));
+entregado.push(new Billete(10, 0));
+entregado.push(new Billete(5, 0));
+entregado.push(new Billete(1, 0));
+
+var caja = [];
+caja.push(new Billete(200, 50));
+caja.push(new Billete(100, 100));
+caja.push(new Billete(50, 200));
+caja.push(new Billete(20, 500));
+caja.push(new Billete(10, 1000));
+caja.push(new Billete(5, 2000));
+caja.push(new Billete(1, 1000));
 
 var dinero = 0;
 var div = 0;
 var papeles = 0;
+var quedan = 0;
+var mostrando = 0;
 
 var b = document.getElementById("extraer");
 b.addEventListener("click", entregarDinero);
 var resultado = document.getElementById("resultado");
+var restantes = document.getElementById("queda");
+var bn = document.getElementById("bandeja");
+var bandeja = bn.getContext("2d");
+
+
+function entregarDinero() {
+
+  var t = document.getElementById("dinero");
+  dinero = parseInt(t.value);
+
+  for (bi of caja) {
+    if (dinero > 0) {
+      div = Math.floor(dinero / bi.valor);
+      if (div > bi.cantidad) {
+        papeles = bi.cantidad;
+      } else {
+        papeles = div;
+      }
+      for (en of entregado) {
+        if (bi.valor == en.valor) {
+          en.cantidad = papeles;
+        }
+      }
+      bi.cantidad = bi.cantidad - papeles;
+      dinero = dinero - (bi.valor * papeles);
+    }
+  }
+
+  for (q of caja) {
+  quedan = quedan + (q.valor * q.cantidad)
+  }
+  restantes.innerHTML= "Aún quedan $" + quedan;
+  quedan = 0;
+
+  if (dinero > 0) {
+    mostrando = "No puedo darte esa cantidad";
+  }
+  else {
+    for(var e of entregado){
+      if (e.cantidad > 0) {
+      mostrando = mostrando + e.cantidad + " billetes de $" + e.valor + "<br />";
+      }
+    }
+  }
+  resultado.innerHTML = mostrando;
+  mostrando = 0;
+
+  bandeja.clearRect(0, 0, 325, 150);
+  var y = 0;
+  var x = 0;
+  for (dibujo of entregado) {
+    for (var dib = 0; dib < dibujo.cantidad; dib ++){
+      bandeja.drawImage(dibujo.imagen, x, y);
+      x += 6;
+      y += 3;
+    }
+  }
+  for (ent of entregado) {
+    ent.cantidad = 0;
+  }
+}
